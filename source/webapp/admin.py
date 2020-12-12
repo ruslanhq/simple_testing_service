6d3498387eb25d7974a0e7f0391dce99fcdbc547
ruslanhq <h.ruslan97@gmail.com>
2020-12-12 18:57:50 +0600

    Created models, made migrations and registred in admin.py

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from webapp.models import Test, Question, Answer
+
+admin.site.register(Test)
+admin.site.register(Question)
+admin.site.register(Answer)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+
+class Test(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название')
+    description = models.TextField(max_length=250, verbose_name='Описание', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Тест'
+        verbose_name_plural = 'Тесты'
+
+
+class Question(models.Model):
+    test = models.ForeignKey(Test, related_name='questions', verbose_name='Тест', on_delete=models.CASCADE)
+    text = models.TextField(max_length=250, verbose_name='Вопрос')
+
+    def __str__(self):
+        return self.text
+
+    class Meta:
+        verbose_name = 'Вопрос'
+        verbose_name_plural = 'Вопросы'
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, related_name='answers', verbose_name='Вопрос',
+                                 on_delete=models.CASCADE)
+    text = models.TextField(max_length=200, verbose_name='Ответ')
+    is_valid = models.BooleanField(default=False, verbose_name='Верный ответ')
+
+    def __str__(self):
+        return '"{}" - {}'.format(self.text, 'valid' if self.is_valid else 'invalid')
+
+    class Meta:
+        verbose_name = 'Ответ'
+        verbose_name_plural = 'Ответы'
+
